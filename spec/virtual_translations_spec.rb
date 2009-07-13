cfc622e94d52ad4f5e2ed9db203581ee92b4fda9
grosser <grosser.michael@gmail.com>
2009-07-13 07:36:50 +0200

    specs for storing

--- spec/virtual_translations_spec.rb
+++ spec/virtual_translations_spec.rb
@@ -41,7 +41,56 @@ describe 'Virtual Translations' do
   end
 
   describe 'storing' do
-    it "is not stored when validations fail"
-    it "is stored after save"
+    it "stores nothing when nothing was set" do
+      lambda{ Product.create! }.should_not change(Translation, :count)
+      Product.last.title.should == nil
+    end
+
+    it "stores column as translation on save" do
+      lambda{ Product.create!(:title=>'x') }.should change(Translation, :count).by(+1)
+    end
+
+    it "stores nothing when blank was set" do
+      lambda{ Product.create! :title=>'   ' }.should_not change(Translation, :count)
+      Product.last.title.should == nil
+    end
+    
+    it "creates no translation when validations fail" do
+      p = Product.new :title=>'xxx'
+      p.should_receive(:valid?).and_return false
+      lambda{ p.save }.should_not change(Translation, :count)
+    end
+
+    it "is stored after save" do
+      p = Product.create!(:title=>'1')
+      p.title = '2'
+      p.save!
+      Product.last.title.should == '2'
+    end
+
+    it "updates the existing translation on change" do
+      p = Product.create!(:title=>'1')
+      lambda{
+        p.title = '2'
+        p.save!
+      }.should_not change(Translation, :count)
+    end
+
+    it "deletes the existing translation when changing to blank" do
+      p = Product.create!(:title=>'1')
+      lambda{
+        p.title = ''
+        p.save!
+      }.should change(Translation, :count).by(-1)
+      Product.last.title.should == nil
+    end
+
+    it "can store multiple translations at once" do
+      lambda{
+        Product.create!(:title_in_de=>'Hallo', :title_in_en=>'Hello')
+      }.should change(Translation, :count).by(+2)
+      Product.last.title_in_de.should == 'Hallo'
+      Product.last.title.should == 'Hello'
+    end
   end
 end
